b6f3d8d6c4b2cd3887a738f0b1ae531fa915b520
Pedro Medeiros <pedrincjm@gmail.com>
2020-02-19 21:16:40 -0300

    Novos aquivos para área

---
+++ Area/Area/Areas/Teste/TesteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Area.Areas.Teste
+{
+    public class TesteAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Teste";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Teste_default",
+                "Teste/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

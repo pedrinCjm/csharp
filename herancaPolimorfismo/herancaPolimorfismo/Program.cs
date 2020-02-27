f6cb80e0c3bc3f1ffb26889e27c50028ac2f3900
Pedro Medeiros <pedrincjm@gmail.com>
2020-02-27 19:47:54 -0300

    Criação do projeto: Herança e polimorfismo

---
+++ herancaPolimorfismo/herancaPolimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace herancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

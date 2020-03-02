cbaf12afd078b7a7874bb7b853fc15daa3ad6b2c
Pedro Medeiros <pedrincjm@gmail.com>
2020-03-02 19:39:31 -0300

    Criando o projeto de manipulação de arquivos

---
+++ manipulacao_de_arquivos/manipulacao_de_arquivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace manipulacao_de_arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manipulação de arquivos em C#!");
+            Console.WriteLine("02/03/2020 - Pedro Medeiros - Graduado em engenharia da computação - Ufop");
+            Console.WriteLine();
+
+            string sourcePath = @"D:\csharp\manipulacao_de_arquivos\manipulacao_de_arquivos\arquivos_de_teste\sourcePath.txt";
+            string targetPath = @"D:\csharp\manipulacao_de_arquivos\manipulacao_de_arquivos\arquivos_de_teste\targetPath.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath); // cria uma instância do arquivo
+                fileInfo.CopyTo(targetPath); // faz uma cópia do arquivo para esse outro
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}

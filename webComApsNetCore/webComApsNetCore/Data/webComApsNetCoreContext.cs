87f5af51258bea4dc35823dd0e08204e088a8c81
Pedro Medeiros <pedrincjm@gmail.com>
2020-03-16 19:20:12 -0300

    recriando webComApsNetCore

---
+++ webComApsNetCore/webComApsNetCore/Data/webComApsNetCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webComApsNetCore.Models;
+
+namespace webComApsNetCore.Data
+{
+    public class webComApsNetCoreContext : DbContext
+    {
+        public webComApsNetCoreContext (DbContextOptions<webComApsNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webComApsNetCore.Models.Department> Department { get; set; }
+    }
+}
